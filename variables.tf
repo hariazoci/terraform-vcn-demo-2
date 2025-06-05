e94be108587b332c40ec91241e10320ea485a945
hariazoci <167167973+hariazoci@users.noreply.github.com>
2025-06-05 15:34:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -53,6 +53,12 @@ variable "subnet_cidr" {
   default     = "10.0.1.0/24"
 }
 
+variable "availability_domain " {
+  description = "ad"
+  type        = string
+  default     = "AD-1"
+}
+
 /*
 variable "subnet2_cidr" {
   description = "Subnet CIDR block"
